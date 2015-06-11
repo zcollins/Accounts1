a963bc084228ba705823ee891428e5419e59009a
Zachary Collins <zachary.collins@hansoninc.com>
2015-06-11 19:30:39 -0400

    init edit 6/11

--- Objects/Account.php
+++ Objects/Account.php
@@ -67,3 +67,4 @@ class Account {
         return $this->password;
     }
 }
+
--- init.php
+++ init.php
@@ -1,8 +1,18 @@
 <?php
 
-echo 'test';
 
 $account = \Accounts\Account::create('zcollins', '123');
 
 $account->save();
 
+//$json_string =    file_get_contents("creds.json");              //reads the file
+//$parsed_json = json_decode($json_string, true);                 //decoedes
+//$parsed_json = $parsed_json['forecast']['txt_forecast']['forecastday'];
+////pr($parsed_json);
+//
+//foreach($parsed_json as $key => $value)
+//{
+//    echo $value['period'] . '<br>';
+//    echo $value['icon'] . '<br>';
+//    // etc
+//}
