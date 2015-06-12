9726af0477aa5ee92e408158b7caecb7fb82daf8
Zachary Collins <zachary.collins@hansoninc.com>
2015-06-11 21:00:39 -0400

    eliminate echo tests

--- init.php
+++ init.php
@@ -3,6 +3,6 @@ ini_set('display_errors', 1);
 
 error_reporting(E_ALL);
 
-$account = Accounts\Account::create('zcollins', '123');
+$account = \Accounts\Account::create('zcollins', '123');
 
 $account->save();
